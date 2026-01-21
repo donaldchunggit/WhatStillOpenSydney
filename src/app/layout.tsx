cfb60e3f252184de01368c2557f938d5592ff35a
donny chung <donnychung92@gmail.com>
2026-01-21 16:35:26 +1100

    add: image pops up when copy pasting link to make it seem more credible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,33 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "What Still Open Sydney",
-  description: "Find venues open at a given time in Sydney.",
+  description: "Find venues that are open in Sydney at any time.",
+
+  openGraph: {
+    title: "What Still Open Sydney",
+    description: "Find venues that are open in Sydney right now.",
+    url: "https://what-still-open-sydney.vercel.app/",
+    siteName: "What Still Open Sydney",
+    images: [
+      {
+        url: "/whatstillopensydney.png",
+        width: 1200,
+        height: 630,
+        alt: "What Still Open Sydney",
+      },
+    ],
+    locale: "en_AU",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "What Still Open Sydney",
+    description: "Find venues that are open in Sydney right now.",
+    images: ["/whatstillopensydney.png"],
+  },
 };
 
-// This is the key mobile-friendly setting
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
@@ -14,7 +37,9 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
